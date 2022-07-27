68936e0043e7b7067bc65022aa0f7d848cd2e0e9
masatotezuka <tez.0731.tez@gmail.com>
2022-07-27 23:50:22 +0900

    add parts Button

---
+++ frontend/src/components/common/parts/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyledButton } from './styledButton';
+
+type Props = {
+  text: string;
+  onClick: () => void;
+  type?: 'submit' | 'button' | 'reset';
+};
+
+export const Button = (props: Props) => {
+  return (
+    <StyledButton onClick={props.onClick} type={props.type}>
+      {props.text}
+    </StyledButton>
+  );
+};
