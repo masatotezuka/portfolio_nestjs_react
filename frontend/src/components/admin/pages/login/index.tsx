5ef31051c030a28205d80d40e37478e6172af28d
masatotezuka <tez.0731.tez@gmail.com>
2022-07-27 16:50:11 +0900

    update login page

--- frontend/src/components/admin/pages/login/index.tsx
+++ frontend/src/components/admin/pages/login/index.tsx
@@ -8,20 +8,21 @@ export const LoginPage = () => {
     <Wrapper>
       <Container>
         <InputsContainer>
-          <InputContainer>
-            <Label>メールアドレス</Label>
-            <InputText type="text"></InputText>
-          </InputContainer>
-          <InputContainer>
-            <Label>パスワード</Label>
-            <InputText type="text"></InputText>
-          </InputContainer>
+          <LabeledInputText
+            type="email"
+            text="メールアドレス"
+            forName="login_email"
+          ></LabeledInputText>
+          <LabeledInputText
+            type="password"
+            text="パスワード"
+            forName="login_password"
+          ></LabeledInputText>
         </InputsContainer>
         <ButtonWrapper>
           <Card path="/home" text="ログイン"></Card>
-          <Button>ログイン</Button>
-          <Button>パスワードを変更する</Button>
-          <Button>管理者を登録</Button>
+          <Card path="/reset-password" text="パスワードを変更する"></Card>
+          <Card path="/register" text="管理者を登録"></Card>
         </ButtonWrapper>
       </Container>
     </Wrapper>
@@ -44,24 +45,6 @@ const InputsContainer = styled.div`
   flex-direction: column;
 `;
 
-const InputContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 0px 10px 0px;
-`;
-
-const InputText = styled.input`
-  display: flex;
-  width: 350px;
-  padding: 5px;
-`;
-
-const Label = styled.label`
-  font-size: 16px;
-  color: #000000;
-  padding-right: 20px;
-`;
-
 const ButtonWrapper = styled.div`
   margin-top: 40px;
   display: flex;
@@ -70,18 +53,4 @@ const ButtonWrapper = styled.div`
   justify-content: space-between;
   align-content: center;
   height: 250px;
-`;
-
-const Button = styled.button`
-  border-radius: 10px;
-  font-weight: bold;
-  background-color: rgba(91, 134, 200, 1);
-  color: rgba(255, 255, 255, 1);
-  text-align: center;
-  border: none;
-  width: 200px;
-  height: 50px;
-  &:hover {
-    cursor: pointer;
-  }
 `;
