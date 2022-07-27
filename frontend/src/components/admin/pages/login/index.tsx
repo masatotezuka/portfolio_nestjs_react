92ba4e1833de327e50ff88efe57cd21db1bfd062
masatotezuka <tez.0731.tez@gmail.com>
2022-07-27 16:46:15 +0900

    add part labeledinputtext

--- frontend/src/components/admin/pages/login/index.tsx
+++ frontend/src/components/admin/pages/login/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { Card } from '../../../common/parts/Card/Card';
+import { LabeledInputText } from '../../../common/parts/InputText/LabeledInputText';
 
 export const LoginPage = () => {
   return (
