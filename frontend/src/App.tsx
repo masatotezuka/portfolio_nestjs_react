57f8add375cb37fbcfbb91a58fc12fa9e6227b6a
masatotezuka <tez.0731.tez@gmail.com>
2022-07-30 10:21:26 +0900

    add resetPassword page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import { EmployeeListPage } from './components/admin/pages/employeeList';
 import { HomePage } from './components/admin/pages/home';
 import { LoginPage } from './components/admin/pages/login';
+import { ResetPasswordPage } from './components/admin/pages/password/resetPassword';
 import { RegisterOrganizationPage } from './components/admin/pages/registerOrganization';
+
 export const App = () => {
   return (
     <Routes>
@@ -13,6 +15,7 @@ export const App = () => {
       ></Route>
       <Route path="/employee" element={<EmployeeListPage />}></Route>
       <Route path="/home" element={<HomePage />}></Route>
+      <Route path="/reset-password" element={<ResetPasswordPage />}></Route>
     </Routes>
   );
 };
