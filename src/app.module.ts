b6b5e68e46e1c46c86f9a0eb6438ce5b4f9772bc
masatotezuka <tez.0731.tez@gmail.com>
2022-08-04 16:01:15 +0900

    add: connect psql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'my_dev',
+      password: undefined,
+      database: 'portfolio_app__development',
+      synchronize: true,
+      logging: ['query', 'error'],
+      entities: ['./entity/*.ts'],
+      migrations: ['./migration/*.ts'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
