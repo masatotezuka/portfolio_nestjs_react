de6a2aea872ba3e0194f3b1bc27a18770d4ee8fe
masatotezuka <tez.0731.tez@gmail.com>
2022-08-09 23:28:36 +0900

    fix: dto

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,12 +1,22 @@
-import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
 export class CreateAdminDto {
+  @IsString()
+  @IsNotEmpty()
   organizationName: string;
 
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
 
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
 
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
