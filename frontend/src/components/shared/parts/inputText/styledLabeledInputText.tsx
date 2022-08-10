acb46ab89aedc45789da4f712b08c700975d78da
masatotezuka <tez.0731.tez@gmail.com>
2022-08-10 18:53:23 +0900

    fix:style

--- frontend/src/components/shared/parts/inputText/styledLabeledInputText.tsx
+++ frontend/src/components/shared/parts/inputText/styledLabeledInputText.tsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export const StyledInputContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -17,4 +22,11 @@ export const StyledLabel = styled.label<{ width: string }>`
   font-size: 16px;
   width: ${(props) => props.width};
   color: #000000;
+`;
+
+export const ErrorMessage = styled.p`
+  font-size: 12px;
+  margin-top: 5px;
+  color: red;
+  text-align: right;
 `;
