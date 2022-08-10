2dde9a30de1b5d63117fc95996c8a79c4792ced3
masatotezuka <tez.0731.tez@gmail.com>
2022-08-10 18:54:22 +0900

    fix: jwt expiresIn and import User Entity

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,14 +7,17 @@ import { jwtConstants } from './constants';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtAuthGuard } from './guard/jwt-auth.guard';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     UserModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: 60 * 60 * 48 },
+      signOptions: { expiresIn: 60 * 30 },
     }),
   ],
   providers: [AuthService, JwtStrategy, JwtAuthGuard],
