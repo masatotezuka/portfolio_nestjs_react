7300f26c7ff5dd46e22ee29195235b315f12d654
masatotezuka <tez.0731.tez@gmail.com>
2022-08-17 11:48:36 +0900

    fix: port number

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -89,7 +89,7 @@ export class UserService {
     user.verificationTokenExpiredAt = verificationTokenExpiredAt;
     await this.userRepository.save(user);
 
-    const verificationTokenUrl = `http://localhost:8000/${
+    const verificationTokenUrl = `http://localhost:3000/${
       user.verificationToken
     }-${format(verificationTokenExpiredAt, 'yyyy-MM-dd')}`;
 
