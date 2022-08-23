8f56a3666b09cca5e4323c5692d4c095b09dd464
masatotezuka <tez.0731.tez@gmail.com>
2022-08-23 12:33:16 +0900

    add: getAccessTokenFromCookie

---
+++ frontend/src/features/util/index.ts
@@ -0,0 +1,16 @@
+export function getAccessTokenFromCookie() {
+  const cookies = document.cookie;
+  const cookie: { accessToken: string } = { accessToken: '' };
+  if (cookies !== '') {
+    const temp = cookies.split(';');
+    for (let i = 0; i < temp.length; i++) {
+      const data = temp[i].split('=');
+      let key = data[0];
+      let value = data[1];
+      if (key === 'accessToken') {
+        cookie.accessToken = value;
+      }
+    }
+  }
+  return cookie.accessToken;
+}
