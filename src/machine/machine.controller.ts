9741989aade9d0969817a7153b2a497d22b72433
masatotezuka <tez.0731.tez@gmail.com>
2022-09-04 11:22:12 +0900

    fix: createMachine

--- src/machine/machine.controller.ts
+++ src/machine/machine.controller.ts
@@ -1,28 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateMachineDto } from './machine.dto';
 import { MachineService } from './machine.service';
+import { MachineItems } from './machine.interface';
 
 @Controller('machines')
 export class MachineController {
   constructor(private readonly machineService: MachineService) {}
   @Post()
-  async create(@Body() createMachineDto: CreateMachineDto) {
+  async create(
+    @Body() createMachineDto: CreateMachineDto,
+  ): Promise<MachineItems> {
     return await this.machineService.createMachine(createMachineDto);
   }
 }
-type User = {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-};
-
-type Machine = {
-  id?: number;
-  category: string;
-  name: string;
-  purchasedAt?: Date | string;
-  user: User;
-  updateAt: Date;
-  usageStatus: string;
-};
--- src/machine/machine.service.ts
+++ src/machine/machine.service.ts
@@ -4,8 +4,9 @@ import { MachineHistory } from 'src/entity/machine-history.entity';
 import { Machine } from 'src/entity/machine.entity';
 import { UserMachine } from 'src/entity/user-machine.entity';
 import { User } from 'src/entity/user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateMachineDto } from './machine.dto';
+import { MachineItems } from './machine.interface';
 
 @Injectable()
 export class MachineService {
@@ -21,12 +22,18 @@ export class MachineService {
 
     @InjectRepository(UserMachine)
     private userMachineRepository: Repository<UserMachine>,
+
+    private AppDataSource: DataSource,
   ) {}
-  async createMachine(createMachineDto: CreateMachineDto) {
-    const { category, name, purchasedAt, userId, usageStatus } =
+
+  async createMachine(
+    createMachineDto: CreateMachineDto,
+  ): Promise<MachineItems> {
+    const { symbol, category, name, purchasedAt, userId, usageStatus } =
       createMachineDto;
 
     const machine = this.machineRepository.create({
+      symbol: symbol,
       category: category,
       name: name,
       purchasedAt: purchasedAt,
@@ -36,41 +43,78 @@ export class MachineService {
       usageStatus: usageStatus,
     });
     const userMachine = this.userMachineRepository.create();
-
     machineHistory.machine = machine;
     userMachine.machine = machine;
-    const newMachine = await this.machineRepository.save(machine);
 
-    if (userId) {
-      const user = await this.userRepository.findOne({
-        where: {
-          id: userId,
-        },
-      });
-      machineHistory.user = user;
-      userMachine.user = user;
-      userMachine.machine = machine;
-      await this.userMachineRepository.save(userMachine);
+    const result = await this.AppDataSource.transaction(
+      async (transactionalEntityManager) => {
+        const machineRepository =
+          transactionalEntityManager.getRepository(Machine);
+        const userMachineRepository =
+          transactionalEntityManager.getRepository(UserMachine);
+        const machineHistoryRepository =
+          transactionalEntityManager.getRepository(MachineHistory);
+
+        const newMachine = await machineRepository.save(machine);
+
+        if (userId) {
+          const user = await this.userRepository.findOne({
+            where: {
+              id: userId,
+            },
+          });
+          machineHistory.user = user;
+          userMachine.user = user;
+          userMachine.machine = machine;
+          await userMachineRepository.save(userMachine);
+        }
+        await machineHistoryRepository.save(machineHistory);
+        return await machineRepository.findOne({
+          select: {
+            id: true,
+            symbol: true,
+            name: true,
+            category: true,
+            purchasedAt: true,
+            machineHistories: { usageStatus: true, updateAt: true },
+            userMachines: {
+              id: true,
+              user: { id: true, firstName: true, lastName: true },
+            },
+          },
+          relations: { machineHistories: true, userMachines: { user: true } },
+          where: { id: newMachine.id },
+        });
+      },
+    );
+
+    if (!result.userMachines[0]) {
+      const response = {
+        id: result.id,
+        symbol: result.symbol,
+        name: result.name,
+        category: result.category,
+        purchasedAt: result.purchasedAt,
+        usageStatus: result.machineHistories[0].usageStatus,
+        updateAt: result.machineHistories[0].updateAt,
+      };
+      return response;
     }
 
-    await this.machineHistoryRepository.save(machineHistory);
-    //machineId name category purchasedAt user{userId/firstName/lastName} usageStatus machineHistory updateAt
-    const response = await this.machineRepository.findOne({
-      select: {
-        id: true,
-        name: true,
-        category: true,
-        purchasedAt: true,
-        machineHistories: { usageStatus: true, updateAt: true },
-        userMachines: {
-          id: true,
-          user: { id: true, firstName: true, lastName: true },
-        },
+    const response = {
+      id: result.id,
+      symbol: result.symbol,
+      name: result.name,
+      category: result.category,
+      purchasedAt: result.purchasedAt,
+      usageStatus: result.machineHistories[0].usageStatus,
+      updateAt: result.machineHistories[0].updateAt,
+      user: {
+        id: result.userMachines[0].user.id,
+        firstName: result.userMachines[0].user.firstName,
+        lastName: result.userMachines[0].user.lastName,
       },
-      relations: { machineHistories: true, userMachines: { user: true } },
-      where: { id: newMachine.id },
-    });
-
+    };
     return response;
   }
 }
