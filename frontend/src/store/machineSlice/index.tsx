c2eee1d837e3df20097532c57679b5f263d1a51f
masatotezuka <tez.0731.tez@gmail.com>
2022-09-18 16:34:53 +0900

    fix: machineSlice

--- frontend/src/store/machineSlice/index.tsx
+++ frontend/src/store/machineSlice/index.tsx
@@ -1,6 +1,6 @@
 import {
   createAsyncThunk,
-  createSelector,
+  createEntityAdapter,
   createSlice,
 } from '@reduxjs/toolkit';
 import { RootState } from '..';
@@ -8,48 +8,47 @@ import {
   createAdminMachine,
   fetchAdminMachinesByUserId,
 } from '../../features/api';
-import { MachineItem, CreateMachine } from '../../features/types';
+import { Machine, CreateMachine } from '../../features/types';
 
-type InitialState = {
-  machines: MachineItem[];
-  status: 'idle' | 'pending' | 'fulfilled' | 'rejected';
-};
-
-const initialState: InitialState = {
-  machines: [],
-  status: 'idle',
-};
+const machineAdapter = createEntityAdapter<Machine>();
+const machineInitialEntityState = machineAdapter.getInitialState({
+  status: "'idle",
+});
 
 export const createMachine = createAsyncThunk(
   'machines/create',
-  async (machine: CreateMachine): Promise<MachineItem> => {
+  async (machine: CreateMachine,): Promise<Machine> => {
     return await createAdminMachine(machine);
   },
 );
 
 export const fetchMachines = createAsyncThunk(
   'machine/fetch',
-  async (): Promise<MachineItem[]> => {
+  async (): Promise<Machine[]> => {
     return await fetchAdminMachinesByUserId();
   },
 );
 
 const machineSlice = createSlice({
   name: 'machines',
-  initialState,
-  reducers: {},
+  initialState: machineInitialEntityState,
+  reducers: {
+    addMachine: (state, action) => {
+      return machineAdapter.addOne(state, action.payload);
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(createMachine.fulfilled, (state, action) => {
         state.status = 'fulfilled';
-        state.machines.push(action.payload);
+        machineAdapter.addOne(state, action.payload);
       })
       .addCase(createMachine.rejected, (state, action) => {
         state.status = 'rejected';
       })
       .addCase(fetchMachines.fulfilled, (state, action) => {
+        machineAdapter.upsertMany(state, action.payload);
         state.status = 'fulfilled';
-        state.machines = action.payload;
       })
       .addCase(fetchMachines.rejected, (state, action) => {
         state.status = 'rejected';
@@ -57,7 +56,9 @@ const machineSlice = createSlice({
   },
 });
 
-export const machineItemsSelector = (state: RootState) =>
-  state.machine.machines;
+export const { selectAll: selectAllMachines, selectById: selectMachineById } =
+  machineAdapter.getSelectors((state: RootState) => state.machines);
+
+export const { addMachine } = machineSlice.actions;
 
 export default machineSlice.reducer;
