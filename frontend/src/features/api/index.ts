967417c61c59f1b907975a970c6acdc0b6a58b7b
masatotezuka <tez.0731.tez@gmail.com>
2022-09-18 16:35:35 +0900

    fix: api file

--- frontend/src/features/api/index.ts
+++ frontend/src/features/api/index.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import { BASE_URL } from '../constants/api';
 import {
   Login,
   Admin,
   RequestPasswordReset,
   VerifyPassword,
   CreateMachine,
-  MachineItem,
+  Machine,
   Employee,
 } from '../types';
 import { getAccessTokenFromCookie } from '../utils/getAccessTokenFromCookie';
@@ -28,57 +29,51 @@ const adminId = getAdminIdFromCookie();
 export const login = async (
   data: Login,
 ): Promise<{ accessToken: string; userId: number }> => {
-  const response = await axios.post(`http://localhost:8000/auth/login`, data);
+  const response = await axios.post(`${BASE_URL}auth/login`, data);
   return response.data;
 };
 
 export const signUp = async (data: Admin) => {
-  const response = await axios.post(`http://localhost:8000/users/admin`, data);
+  const response = await axios.post(`${BASE_URL}users/admin`, data);
   return response.data.accessToken;
 };
 
 export const requestPasswordReset = async (data: RequestPasswordReset) => {
-  return await axios.put(
-    `http://localhost:8000/users/password-reset/request`,
-    data,
-  );
+  return await axios.put(`${BASE_URL}users/password-reset/request`, data);
 };
 
 export const verifyPassword = async (data: VerifyPassword) => {
   return await axios.patch(
-    `http://localhost:8000/users/password-reset/verification`,
+    `${BASE_URL}users/password-reset/verification`,
     data,
   );
 };
 
 export const verifyAccessToken = async () => {
-  const response = await axios.get('http://localhost:8000/auth/verification');
+  const response = await axios.get(`${BASE_URL}auth/verification`);
   return response.statusText;
 };
 
 export const createAdminMachine = async (
   data: CreateMachine,
-): Promise<MachineItem> => {
-  const response = await axios.post(
-    `http://localhost:8000/machines/${adminId}`,
-    data,
-  );
+): Promise<Machine> => {
+  const response = await axios.post(`${BASE_URL}machines/${adminId}`, data);
   return response.data;
 };
 
-export const fetchAdminMachinesByUserId = async (): Promise<MachineItem[]> => {
-  const response = await axios.get(`http://localhost:8000/machines/${adminId}`);
+export const fetchAdminMachinesByUserId = async (): Promise<Machine[]> => {
+  const response = await axios.get(`${BASE_URL}machines/${adminId}`);
   return response.data;
 };
 
 export const fetchEmployeesAdmin = async () => {
-  const response = await axios.get(`http://localhost:8000/users`);
+  const response = await axios.get(`${BASE_URL}users`);
   return response.data;
 };
 
 export const createEmployeeAdmin = async (data: Employee) => {
   try {
-    const response = await axios.post(`http://localhost:8000/users`, data);
+    const response = await axios.post(`${BASE_URL}users`, data);
     console.log('success');
     return response.data;
   } catch (error) {
@@ -87,11 +82,11 @@ export const createEmployeeAdmin = async (data: Employee) => {
 };
 
 export const deleteEmployeeAdmin = async (userId: number) => {
-  const response = await axios.delete(`http://localhost:8000/users/${userId}`);
+  const response = await axios.delete(`${BASE_URL}users/${userId}`);
   return response.data;
 };
 
 export const updateEmployeeAdmin = async (data: Employee) => {
-  const response = await axios.patch(`http://localhost:8000/users`, data);
+  const response = await axios.patch(`${BASE_URL}users`, data);
   return response.data;
 };
