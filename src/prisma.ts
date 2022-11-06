046a2916079d8dab466917456ad55a543036555f
masatotezuka <tez.0731.tez@gmail.com>
2022-11-06 15:38:28 +0900

    add soft delete in middleware

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  if (params.action === 'delete') {
+    params.action = 'update';
+    params.args['data'] = { deletedAt: new Date() };
+  } else if (params.action === 'deleteMany') {
+    params.action = 'updateMany';
+    params.args['data'] = { deletedAt: new Date() };
+  }
+  return next(params);
+});
